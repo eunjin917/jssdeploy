e06778a1f0781395322ef4a9a9ae32d7c2d30003
eunjin917 <cgoya01@likelion.org>
2020-06-08 19:40:36 +0900

    for deploy lecture

--- jasoseolproject/jasoseolproject/settings.py
+++ jasoseolproject/jasoseolproject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASH_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
 
